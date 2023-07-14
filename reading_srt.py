5aa341e7fa3ac09c2e189fe1fc14714175d87e02
DELL <humairashirzad0@gmail.com>
2023-07-14 13:08:33 +0430

    added reading_srt.py

---
+++ reading_srt.py
@@ -0,0 +1,30 @@
+# Open and read the SRT file
+with open('C:\\Users\\DELL\\Desktop\\How.I.Met.Your.Mother.S01E01.srt', 'r') as srt_file:
+    srt_content = srt_file.read()
+
+# Split the content into words
+words = srt_content.split()
+
+# Create a list to store words without special characters
+clean_words = []
+for word in words:
+    if word.isalpha():
+        clean_words.append(word)
+
+# Open and read the Longman Communication 3000 file
+with open('C:\\Users\\DELL\\Desktop\\Longman Communication 3000.txt', 'r') as lc_file:
+    lc_content = lc_file.read()
+
+# Find similar words between the SRT file and Longman Communication 3000 file
+similar_words = []
+lc_words = lc_content.split()
+for clean_word in clean_words:
+    for lc_word in lc_words:
+        if clean_word == lc_word:
+            similar_words.append(lc_word)
+
+# Remove duplicates by converting the list to a set
+synonyms = set(similar_words)
+
+# Print the set of similar words
+print(synonyms)
